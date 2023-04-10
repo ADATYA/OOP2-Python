d61c97f569ad9ceac288d5a3323d1bc50a4fbec9
bikrom <bikromroy0711@gmail.com>
2023-04-10 15:03:18 +0600

    first commit

---
+++ c-1 class object.py
@@ -0,0 +1,23 @@
+# Class : class is a [blueprint].
+# Shudu class banale hobe nah tar ekta object banano lagbe then oita call korte hobe.
+# Akti class er [multiple object] thke.
+# [camel] case : BikromRoy
+# #class err vator jaita banai take [method]bole 
+
+class Car:
+    a=10
+# Method/funtion
+    
+    def add(self):     # Class er vator function banate gele akta argument pass korte hobe [self] / eri akta object err moto kaj kore.
+        print("Bikrom is learning OOP")
+    
+    #    [ for i in Car:   TypeError: 'type' object is not iterable
+    #         print(i)]
+          
+
+# create a class object
+Car1=Car()
+Car2=Car()
+print (Car.a)
+print(Car2.a)
+Car1.add()  #  object diye printt korbo
---
+++ c-2Method constructor.py
@@ -0,0 +1,26 @@
+# Method:
+
+class car:
+    a=10
+    def __init__(self):  # Constructor:ek call nah korrleo print korte pare
+        print("Adatya is learning python")
+
+    def bus(self):
+        print(self.a)
+        self.c = self.a*self.a
+        print(self.c)
+    
+    
+    def truck(self,a,b): ## argument(self,a,b)
+        print(a+b)
+        print("Bikrom is typing code")
+    
+
+obj=car() #class
+obj.bus() #object
+obj.truck(20,30)
+
+
+
+
+
