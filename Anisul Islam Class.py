12b07075b622afeee00aa104e23c61cc6f944588
bikrom <bikromroy0711@gmail.com>
2023-05-26 23:45:20 +0600

    new

--- Anisul Islam Class.py
+++ Anisul Islam Class.py
@@ -1,73 +1,93 @@
-                                    ## Inheritance ##
+#                                     ## Inheritance ##
 
 
-class Car:
-    def engine(self):
-        print("This is engine")
-    def wheel(self):
-        print("This is wheel")
-
- # class
-class BMW(Car):
-    # method
-    def color(self):
-        print("This is white bmw")
-#     def wheel(self):
-#         print("This is bmw wheel")
+# class Car:
 #     def engine(self):
-#         print("This is bmw engine")
-# #object :
-# c = Car()
-# c.engine()
-# c.wheel()
+#         print("This is engine")
+#     def wheel(self):
+#         print("This is wheel")
+
+#  # class
+# class BMW(Car):
+#     # method
+#     def color(self):
+#         print("This is white bmw")
+# #     def wheel(self):
+# #         print("This is bmw wheel")
+# #     def engine(self):
+# #         print("This is bmw engine")
+# # #object :
+# # c = Car()
+# # c.engine()
+# # c.wheel()
 
 
-bmw = BMW() 
-bmw.color()
-bmw.engine()
-y=issubclass(Car,BMW)
-print(y)
+# bmw = BMW() 
+# bmw.color()
+# bmw.engine()
+# y=issubclass(Car,BMW)
+# print(y)
 
 
-## Practic...
+# ## Practic...
 
-class Shape:
+# class Shape:
+#     def __init__(self,name,id):
+#         self.name = name
+#         self.id = id
+#     def area(self):
+#         print("I am area method of shape class")
+
+
+# class Triangle(Shape):
+#     def area(self):
+#         area = 0.5* self.name * self.id
+#         print("area of Triangle :" , area)
+
+
+# class Rectangle(Shape):
+#     def area(self):
+#         area =self.name * self.id
+#         print("area of Rectangle :" , area)
+
+# t1 = Triangle(20,30)
+# t1.area()
+
+# r1 = Rectangle(20,30)
+# r1.area()
+
+
+class Person:
     def __init__(self,name,id):
         self.name = name
         self.id = id
-    def area(Shape):
-        print("I am area method of shape class")
+    def printto(self):
+        print("This is Perrson class")
 
+class Ankur:
+    def printto(self):
+        printto = "His name is Ankur"
+        print("Name is :",printto)
 
-class Triangle(Shape):
-    def area(self):
-        area = 0.5* self.name * self.id
-        print("area of Triangle :" , area)
+class Bikrom:
+    def printto(self):
+        printto = "His name is Bikrom"
+        print("Name is :",printto)
+A = Ankur()
+A.printto()
 
+B=Bikrom()
+B.printto()
 
-class Rectangle(Shape):
-    def area(self):
-        area =self.name * self.id
-        print("area of Rectangle :" , area)
+#                                 ## Method Overriding ##
 
-t1 = Triangle(20,30)
-t1.area()
-
-r1 = Rectangle(20,30)
-r1.area()
-
-
-
-
-                                ## Method Overriding ##
-
-class Devise:
-   def __init__(self): #constructor
-     print("I am a devise class")
+# class Devise:
+#    def __init__(self): #constructor
+#      print("I am a devise class")
      
-class Laptop(Devise):
-    def __init__(self):
-        super().__init__()  
-        print("I am in Laptop class")
+# class Laptop(Devise):
+#     def __init__(self):
+#         super().__init__()  
+#         print("I am in Laptop class")
         
-D = Laptop()
+# D = Laptop()
